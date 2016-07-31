83699b612390b5ff5b707906708a18a5f1f0fffa
leventid <leventid@umich.edu>
2016-07-31 18:36:02 -0400

    Added more on basic layout for characters

--- CK2CharacterModdingApplication/characterCreator.cpp
+++ CK2CharacterModdingApplication/characterCreator.cpp
@@ -1,5 +1,15 @@
 #include "characterCreator.h"
 
+struct characterAttributes {
+    unordered_set<string> maleNames, femaleNames;
+    int currentID;      // The current ID of the latest character
+    int headAge;        // The age of the head of the dynasty
+
+    int getNextID() const {
+        return currentID;
+    }
+};
+
 void CreateCharacters(const string& characterOutputPath) {
     // Get references to singletons
     miscellaneousAttributes& miscAttr = miscellaneousAttributes::get_instance();
@@ -10,17 +20,30 @@ void CreateCharacters(const string& characterOutputPath) {
         ofstream ofs(characterOutputPath + "\\" + to_string(provIDName.first) + " - " + provIDName.second + " - " + "Characters.txt");
 
         if (ofs.is_open()) {
-            unordered_set<string> maleNames, femaleNames;
+            characterAttributes charAttr;
+            charAttr.currentID = settAttr.character_id_from_province_multiplier * provIDName.first;    // IDs headID + 1 and headID + 2 are reserved for the current head of dynasty
 
+            // Create the head
+            createCharacter(true, false, charAttr, ofs);
+
+            // Create the parents
+
+            
 
             ofs.close();
         }
         else {
-            
+            std::cerr << "Failed to create the character file for the province of " + provIDName.second + "\n";
+            throw std::exception();
         }
 
 
     }
 
     return;
+}
+
+void createCharacter(bool isDynastic, bool isFemale, characterAttributes& charAttr, ofstream& ofs) {
+
+
 }
--- CK2CharacterModdingApplication/characterCreator.h
+++ CK2CharacterModdingApplication/characterCreator.h
@@ -6,6 +6,7 @@
 #include <random>
 #include <fstream>
 #include <string>
+#include <stdexcept>
 using std::string;
 using std::ofstream;
 using std::pair;
@@ -14,7 +15,6 @@ using std::random_device;
 using std::mt19937;
 using std::uniform_int_distribution;
 using std::to_string;
-
 
 // Creates all characters for the specified provinces based on the settings selected
 void CreateCharacters(const string& characterOutputPath);
